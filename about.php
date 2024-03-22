9f077af66b6467b49a1c5aa00a54cf2d1034f21d
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-22 16:59:18 +0530

    About Page Build

---
+++ about.php
@@ -0,0 +1,83 @@
+<?php include('header.php') ?>
+
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option spad set-bg" data-setbg="img/breadcrumb-bg.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="breadcrumb__text">
+                    <h2>About us</h2>
+                    <div class="breadcrumb__links">
+                        <a href="index.php">Home</a>
+                        <span>About</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb End -->
+
+<!-- About Section Begin -->
+<section class="about spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="about__pic">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="about__pic__item about__pic__item--large set-bg" data-setbg="img/about/about-1.jpg"></div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="about__pic__item set-bg" data-setbg="img/about/about-2.jpg"></div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="about__pic__item set-bg" data-setbg="img/about/about-3.jpg"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="about__text">
+                    <div class="section-title">
+                        <span>About videograph</span>
+                        <h2>WHo we are?</h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="services__item">
+                                <div class="services__item__icon">
+                                    <img src="img/icons/si-1.png" alt="">
+                                </div>
+                                <h4>Motion graphics</h4>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="services__item">
+                                <div class="services__item__icon">
+                                    <img src="img/icons/si-2.png" alt="">
+                                </div>
+                                <h4>Scriptwriting and editing</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="about__text__desc">
+                        <p>If you hire our team you will get a photography professional to make a custom
+                            photo shoot for your business and, once the project is over.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- About Section End -->
+
+
+<!-- Team Section Begin -->
+<?php include('teamSection.php') ?>
+<!-- Team Section End -->
+<?php include('footer.php') ?>
