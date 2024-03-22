b949d65a8defb24458ec066ceca00a881b338242
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-22 17:22:28 +0530

    Contact Us

---
+++ contact.php
@@ -0,0 +1,78 @@
+<?php include('header.php') ?>
+
+<!-- Breadcrumb Begin -->
+<div class="breadcrumb-option spad set-bg" data-setbg="img/breadcrumb-bg.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="breadcrumb__text">
+                    <h2>Contact us</h2>
+                    <div class="breadcrumb__links">
+                        <a href="index.php">Home</a>
+                        <span>Contact</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb End -->
+
+<!-- Contact Widget Section Begin -->
+<section class="contact-widget spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-md-6 col-md-3">
+                <div class="contact__widget__item">
+                    <div class="contact__widget__item__icon">
+                        <i class="fa fa-map-marker"></i>
+                    </div>
+                    <div class="contact__widget__item__text">
+                        <h4>Address</h4>
+                        <p>Jabalpur</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-md-6 col-md-3">
+                <div class="contact__widget__item">
+                    <div class="contact__widget__item__icon">
+                        <i class="fa fa-phone"></i>
+                    </div>
+                    <div class="contact__widget__item__text">
+                        <h4>Hotline</h4>
+                        <p>+91 8817762774</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-md-6 col-md-3">
+                <div class="contact__widget__item">
+                    <div class="contact__widget__item__icon">
+                        <i class="fa fa-map-marker"></i>
+                    </div>
+                    <div class="contact__widget__item__text">
+                        <h4>Email</h4>
+                        <p>piyushraikwar289@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Widget Section End -->
+
+<!-- Call To Action Section Begin -->
+<section class="contact spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="contact__map">
+                    <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Jabalpur+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps devices</a></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Call To Action Section End -->
+
+<!-- Footer Section Begin -->
+<?php include('footer.php') ?>
