e5d96ef01697f3ce96b130cd68968e8e8dbcbc7b
Piyush Kumar Raikwar <94155141+Piyush289kumar@users.noreply.github.com>
2024-05-31 08:59:00 +0530

    Telephone & Mail a tag Inserted

--- contact.php
+++ contact.php
@@ -1,5 +1,4 @@
-<?php include('header.php') ?>
-
+<?php include ('header.php') ?>
 <!-- Breadcrumb Begin -->
 <div class="breadcrumb-option spad set-bg" data-setbg="img/breadcrumb-bg.jpg">
     <div class="container">
@@ -17,7 +16,6 @@
     </div>
 </div>
 <!-- Breadcrumb End -->
-
 <!-- Contact Widget Section Begin -->
 <section class="contact-widget spad">
     <div class="container">
@@ -36,18 +34,22 @@
             <div class="col-lg-4 col-md-6 col-md-6 col-md-3">
                 <div class="contact__widget__item">
                     <div class="contact__widget__item__icon">
-                        <i class="fa fa-phone"></i>
+                        <a href="tel:+916268560563">
+                            <i class="fa fa-phone"></i>
+                        </a>
                     </div>
                     <div class="contact__widget__item__text">
                         <h4>Phone</h4>
-                        <p>+91 6268560563</p>
+                        <p>+91 62685-60563</p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 col-md-6 col-md-3">
                 <div class="contact__widget__item">
                     <div class="contact__widget__item__icon">
-                        <i class="fa fa-envelope"></i>
+                        <a href="mailto:jayendrashakya093@gmail.com">
+                            <i class="fa fa-envelope"></i>
+                        </a>
                     </div>
                     <div class="contact__widget__item__text">
                         <h4>Email</h4>
@@ -59,20 +61,20 @@
     </div>
 </section>
 <!-- Contact Widget Section End -->
-
 <!-- Call To Action Section Begin -->
 <section class="contact spad">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="contact__map">
-                    <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=H.No.%20152,%20Near%20Aghori%20Baba%20Temple,%20Damohnaka%20Trimurti%20Nagar,%20Jabalpur+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps devices</a></iframe>
+                    <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+                        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=H.No.%20152,%20Near%20Aghori%20Baba%20Temple,%20Damohnaka%20Trimurti%20Nagar,%20Jabalpur+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a
+                            href="https://www.gps.ie/">gps devices</a></iframe>
                 </div>
             </div>
         </div>
     </div>
 </section>
 <!-- Call To Action Section End -->
-
 <!-- Footer Section Begin -->
-<?php include('footer.php') ?>
+<?php include ('footer.php') ?>
